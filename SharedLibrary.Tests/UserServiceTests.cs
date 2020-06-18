6afba4a7a804db66f0ea1bd5f5d8adde46ff0480
adhikari18 <ashok.adhikari18@gmail.com>
2020-06-18 01:01:10 -0400

    check null, check 2 chars sequence

--- SharedLibrary.Tests/UserServiceTests.cs
+++ SharedLibrary.Tests/UserServiceTests.cs
@@ -6,19 +6,25 @@ namespace SharedLibrary.Tests
     public class UserServiceTests
     {
         [Theory]
+        [InlineData("", false)]
+        [InlineData("       ", false)]
+        [InlineData(null, false)]
         [InlineData("1", false)]
         [InlineData("A", false)]
         [InlineData("12345", false)]
         [InlineData("ABCDE", false)]
         [InlineData("ABAB1", false)]
-        [InlineData("AA123", false)]
         [InlineData("ABCD1#", false)]
         [InlineData("12341234", false)]
+        [InlineData("ABAB1234", false)]
         [InlineData("ABCDE1ABCDE1", false)]
         [InlineData("123456789012A", false)]
         [InlineData("ABCDEFGHIJ123", false)]
+        [InlineData("ba90JhGj11C96VdxtaTfEXF0Q0tKyiRG0wWV6bLqJWehEeTrLRE8QXZSvDYg8y0ktPn9kj0z91unD9RJIZaTXv2nQxnqt4a4LYaL", false)]
 
+        [InlineData("AA1234", true)]
         [InlineData("ABCD1", true)]
+        [InlineData("abcd1", true)]
         [InlineData("1234567890AB", true)]
         [InlineData("ABXYZAB1", true)]
         [InlineData("abABc12", true)]
@@ -29,7 +35,6 @@ namespace SharedLibrary.Tests
             var userService = new UserService();
             var result = userService.ValidatePassword(password);
             Assert.Equal(expected, result);
-
         }
     }
 }
--- SharedLibrary/UserService.cs
+++ SharedLibrary/UserService.cs
@@ -18,13 +18,18 @@ namespace SharedLibrary
 
         public bool ValidatePassword(string password)
         {
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
             var isAlphanumericContainingBoth = Regex.IsMatch(password, @"^([a-zA-Z]+[0-9]|[0-9]+[a-zA-Z])[A-Za-z0-9]*$");
             var hasValidLength = password.Length >= 5 && password.Length <= 12;
-            var consecutiveSubsequenceOfAtLeast1Char = Regex.IsMatch(password, @".*(.+)\1.*");
-            //I wasnt clear whether it needed subsequence of at least 1 char or 2 chars.
-            //I assumed it would be invalid even for 1 character if it follows itself.
-            //Eg. it will consider AA1234 as invalid;
-            var isValid = isAlphanumericContainingBoth && hasValidLength && !consecutiveSubsequenceOfAtLeast1Char; 
+            var consecutiveSubsequenceOfAtLeast2Chars = Regex.IsMatch(password, @"(..+)\1");
+
+            //I wasn't clear whether it needed subsequence of at least 1 char or 2 chars.
+            //I assumed it would be invalid for 2+ characters if it follows itself.
+            //Eg. it will consider AA1234 as valid but ABAB1234 as invalid;
+            var isValid = isAlphanumericContainingBoth && hasValidLength && !consecutiveSubsequenceOfAtLeast2Chars; 
             return isValid;
         }
     }
