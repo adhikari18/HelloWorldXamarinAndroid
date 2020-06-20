9f40b4549c559a0c5ca17087038196abdbb55601
devresponses <ashok.adhikari18@gmail.com>
2020-06-20 12:03:44 -0400

    change to username

--- SharedLibrary/Models/UserInfo.cs
+++ SharedLibrary/Models/UserInfo.cs
@@ -10,9 +10,9 @@ namespace SharedLibrary.Models
         public string UserName { get; set; }
         public string Password { get; set; }
 
-        public UserInfo(string user, string password)
+        public UserInfo(string username, string password)
         {
-            UserName = user;
+            UserName = username;
             Password = password;
         }
     }
